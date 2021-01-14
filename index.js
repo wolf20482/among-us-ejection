17f3b0453f3ecb79130f247f62dde6db27af3bd9
wolf20482 <40973492+wolf20482@users.noreply.github.com>
2021-01-14 20:08:10 +0700

    Add the ability to choose maps, fix grammar
    Add ability to choose maps and add MIRA HQ, Polus and The Airship (The Airship is added according to leaked data about new ejection screen)

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-module.exports = function eject(crewmember, impostorsRemain) {
+module.exports = function eject(crewmember, isImpostor, impostorsRemain, map) {
     if (typeof crewmember != 'string') throw new TypeError('Crewmember name must be a string!');
     if (typeof impostorsRemain != 'number') throw new TypeError('Amount of Impostors remain must be a number!');
-        return (`. 　　　。　　　　•　 　ﾟ　　。 　　.
+    if (typeof isImpostor != 'boolean') throw new TypeError('Impostor/Not Impostor must be a boolean!');
+    if (isImpostor === true) {
+        impostor = 'The Impostor'
+    } else {
+        impostor = 'not The Impostor'
+    }
+    if (impostorsRemain > 1) {
+        grammarCompliance = 'Impostors remain'
+    } else {
+        grammarCompliance = 'Impostor remains'
+    }
+    if (map === 'The Skeld') {
+        result = `. 　　　。　　　　•　 　ﾟ　　。 　　.
     　　　.　　　 　　.　　　　　。　　 。　. 　
     .　　 。　　　　　 ඞ  。 . 　　 • 　　　　•
-    　　ﾟ　  ${crewmember} was not The Impostor.　 。　.
-    　　'　　　 ${impostorsRemain} Impostor remains 　 　　。
-    　　ﾟ　　　.　　　. ,　　　　.　 .`)
-  };
+    　　ﾟ　  ${crewmember} was ${impostor}.　 。　.
+    　　'　　　 ${impostorsRemain} ${grammarCompliance} 　 　　。
+    　　ﾟ　　　.　　　. ,　　　　.　 .`
+    } else if (map === 'MIRA HQ') {
+        result = `☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️
+        ☁️☁️☁️☁️☁️ ☁️☁️☁️☁️☁️  ☁️☁️☁️     ☁️☁️☁️☁️
+     ☁️☁️☁️☁️☁️☁️   ඞ ☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️
+            ${crewmember} was ${impostor}
+               ${impostorsRemain} ${grammarCompliance}.`
+  } else if (map === 'Polus') {
+        result = `
+                  ඞ
+            ${crewmember} was ${impostor}
+               ${impostorsRemain} ${grammarCompliance}.
+      🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥`
+  } else {
+        result = `
+                  ඞ
+         ☁️☁️ ${crewmember} was ${impostor}
+   ☁️☁️☁️☁️☁️☁️☁️☁️ ${impostorsRemain} ${grammarCompliance}.
+    ☁️☁️☁️☁️☁️☁️ ☁️☁️☁️☁️☁️☁️☁️    ☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️
+       ☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️   ☁️☁️☁️☁️☁️☁️☁️☁️` // The Airship
+  return result;
